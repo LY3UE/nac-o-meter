4134f31d159831b15634ba00273d31d5b3bc55a9
Simonas Kareiva <simonas@5grupe.lt>
2018-12-15 23:26:54 +0200

    Add not-received reports to the front page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Log;
+use App\Entity\Round;
 use App\Entity\Callsign;
+use App\Entity\QsoRecord;
 use App\Form\CallsignSearch;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,10 +20,24 @@ class HomePageController extends AbstractController
      */
     public function index(Request $request)
     {
-        $repository = $this->getDoctrine()->getRepository(Log::class);
-        $lastCallsigns = $repository->findLastCallsigns(5);
-        $lastDate = $repository->findLastDate()[1];
-        $lastMonthStats = $repository->findLastMonthStats($lastDate);
+        $logRepository = $this->getDoctrine()->getRepository(Log::class);
+        $qsoRepository = $this->getDoctrine()->getRepository(QsoRecord::class);
+        $roundRepository = $this->getDoctrine()->getRepository(Round::class);
+
+        $lastCallsigns = $logRepository->findLastCallsigns(5);
+        $lastDate = $logRepository->findLastDate()[1];
+        $lastMonthStats = $logRepository->findLastMonthStats($lastDate);
+        $lastRounds = $roundRepository->findLastRoundDates($lastDate);
+
+        $logsNotReceived = [];
+        foreach ($lastRounds as $lastRound) {
+          $dateStr = $lastRound['date']->format('Y-m-d');
+          $logsNotReceived[$dateStr] = $qsoRepository->getLogsNotReceived($dateStr);
+        }
+
+        dump($logsNotReceived);
+
+        /* TODO: move below block to a separate route */
 
         $callsignSearchForm = $this->createForm(CallsignSearch::class);
         $callsignSearchForm->handleRequest($request);
@@ -30,7 +46,7 @@ class HomePageController extends AbstractController
             return $this->redirectToRoute(
               'call_search',
               array(
-              'callsign' => $data['callsign']
+                'callsign' => $data['callsign']
             )
           );
         }
@@ -38,7 +54,9 @@ class HomePageController extends AbstractController
         return $this->render('home.html.twig', array(
           'lastMonthStats' => $lastMonthStats,
           'lastDate' => $lastDate,
+          'lastRounds' => $lastRounds,
           'lastCallsigns' => $lastCallsigns,
+          'logsNotReceived' => $logsNotReceived,
           'callSearch' => $callsignSearchForm->createView(),
           'currentYear' => \DateTime::createFromFormat(
             "Y-m-d",
