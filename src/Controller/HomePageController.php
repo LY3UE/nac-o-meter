8e42361c01ef42cad85f3a276bcecea830cea1dc
Simonas Kareiva <simonas@5grupe.lt>
2018-12-23 13:32:18 +0200

    WIP: add TranslatorInterface to HomePageController

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
