225b16e3f5f194f22b3997c943731570b274bef5
Simonas Kareiva <simonas@5grupe.lt>
2018-12-23 13:25:57 +0200

    Fix typo in doctrine schema_filter: add WWLs

---
+++ src/Entity/Wwl.php
@@ -0,0 +1,30 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Wwl - Worldwide locator
+ *
+ * @ORM\Table(name="wwls", uniqueConstraints={@ORM\UniqueConstraint(name="wwl", columns={"wwl"})})
+ * @ORM\Entity(repositoryClass="App\Repository\WwlRepository")
+ */
+class Wwl
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="wwlID", type="smallint", nullable=false, options={"unsigned"=true,"comment"="WWL ID"})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $wwlid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="wwl", type="string", length=6, nullable=false, options={"comment"="WWL"})
+     */
+    private $wwl;
+
+
+}
